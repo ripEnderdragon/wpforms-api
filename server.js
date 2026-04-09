62acfac78efa730b91dff8ee1fbd1997bc55bf45
ripEnderdragon <161038354+ripEnderdragon@users.noreply.github.com>
2026-04-09 22:39:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 🔑 Load Firebase key (we'll add this later)
+
 const serviceAccount = require("/etc/secrets/serviceAccountKey.json");
 
 admin.initializeApp({
